dc981a43465a8ab672925336831092ff5a934b66
William Low <donnyrahardika@gmail.com>
2024-03-27 15:36:39 +0000

    change pango version

--- src/openbox/build.sh
+++ src/openbox/build.sh
@@ -12,8 +12,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Define software versions.
 OPENBOX_VERSION=3.6.1
-PANGO_VERSION=1.52.0
-LIBXRANDR_VERSION=1.5.0
+PANGO_VERSION=1.50.14
+LIBXRANDR_VERSION=1.5.1
 
 # Define software download URLs.
 OPENBOX_URL=http://openbox.org/dist/openbox/openbox-${OPENBOX_VERSION}.tar.xz
