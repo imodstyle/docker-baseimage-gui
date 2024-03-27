a4f7bcf58103e04016345a72ad37e35a14a252a4
William Low <donnyrahardika@gmail.com>
2024-03-27 15:44:49 +0000

    revert pango to 1.49

--- src/openbox/build.sh
+++ src/openbox/build.sh
@@ -12,7 +12,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Define software versions.
 OPENBOX_VERSION=3.6.1
-PANGO_VERSION=1.50.14
+PANGO_VERSION=1.49.4
 LIBXRANDR_VERSION=1.5.1
 
 # Define software download URLs.
